3fca5796c41b3456c8c7ddc316c2171a4686384d
flameSla <39356103+flameSla@users.noreply.github.com>
2024-07-14 23:42:43 +0700

    Create utilities.py 23:42 14/07/2024

---
+++ utilities.py
@@ -0,0 +1,21 @@
+# ====================================
+def print_line(text, inp, end=""):
+    print(text, "".join("{:12.4f}".format(t) for t in inp), end)
+
+
+def print_q(a):
+    print(a["text"])
+    q = a["matrix"]
+    print("  ", "".join("{:^12s}".format(t) for t in ("q1", "q2", "q3", "q4", "q5")))
+    print_line("q1", q[0])
+    print_line("q2", q[1])
+    print_line("q3", q[2])
+    print_line("q4", q[3])
+    print_line("q5", q[4])
+
+
+######################################
+#
+# main
+if __name__ == "__main__":
+    pass
