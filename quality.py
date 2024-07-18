0649f260e12e821b5f4670e06afb264ae4b09eed
flameSla <39356103+flameSla@users.noreply.github.com>
2024-07-18 22:20:00 +0700

    Update quality.py 22:20 18/07/2024

--- quality.py
+++ quality.py
@@ -1,4 +1,5 @@
 import numpy as np
+import locale
 from itertools import product
 
 from modules import mul_q
@@ -13,6 +14,7 @@ from utilities import print_q
 from schemes import scheme_1
 from schemes import scheme_2
 from schemes import scheme_3
+from schemes import scheme_4
 
 
 np.seterr(all="raise")
@@ -39,8 +41,9 @@ def get_the_ratio_v2(x0, scheme, q_level, q_list, debug, log=False):
                 scheme.print1(x0, q_list, tic)
 
             # has "xout" changed in the last tick?
-            if abs(xout_last[q_level] - xout[q_level]) <= 0.000001 and tic > 99:
-                break
+            if abs(xout_last[q_level] - xout[q_level]) <= 0.00000000001:
+                if not all([x == 0.0 for x in xout]) or tic > 99:
+                    break
             else:
                 xout_last = xout
 
@@ -58,7 +61,9 @@ def get_the_ratio_v2(x0, scheme, q_level, q_list, debug, log=False):
 
 
 # ====================================
-def make_a_complete_search(x0, scheme, name_of_the_machines, q_list, q_level_list):
+def make_a_complete_search(
+    x0, scheme, name_of_the_machines, q_list, q_level_list, filename
+):
     s_level = [x > 0 for x in x0].index(True)
 
     def print_res(res, q_level):
@@ -74,42 +79,73 @@ def make_a_complete_search(x0, scheme, name_of_the_machines, q_list, q_level_lis
                 break
 
     i = 0
-    res = [[], [], [], [], []]
-    for q_level in q_level_list:
-        for q in product(*q_list):
-            print(i)
-            i += 1
-            out = get_the_ratio_v2(list(x0), scheme, q_level, q, False)
-            if abs(out["out"][q_level] - 1.0) <= 0.1:
-                res[q_level].append(
-                    [
-                        out["x0"],
-                        out["out"],
-                        "".join(
-                            " {} = {}".format(a, b["text"])
-                            for a, b in zip(name_of_the_machines, q)
-                        ),
-                    ]
-                )
-            else:
-                print(
-                    "\t{} -> out = {}".format(
-                        "".join("{:23s}".format(t["text"]) for t in q),
-                        out["out"][q_level],
+    locale.setlocale(locale.LC_ALL, "")
+
+    max_i = 1
+    for q in q_list:
+        max_i *= len(q)
+    print("number of combinations = {}".format(max_i))
+
+    min_res = {}
+    for q in range(5):
+        min_res[q] = {
+            "x0": 999999999999999.0,
+            "q_level": q,
+            "text": "",
+            "text_csv": "",
+        }
+    with open(filename, mode="w", encoding="utf8") as f:
+        line = "q_level;x0;out;" + ";".join(
+            "{}".format(a) for a in name_of_the_machines
+        )
+        print(line, file=f)
+        for q_level in q_level_list:
+            temp_res = {
+                "x0": 999999999999999.0,
+                "q_level": q_level,
+                "text": "",
+                "text_csv": "",
+            }
+            for q in product(*q_list):
+                if i % 1000 == 0:
+                    print(i)
+                i += 1
+                out = get_the_ratio_v2(list(x0), scheme, q_level, q, False)
+                if abs(out["out"][q_level] - 1.0) <= 0.1:
+                    text_csv = "{};{};{};{}".format(
+                        q_level,
+                        locale.format_string("%f", out["x0"][s_level]),
+                        locale.format_string("%f", out["out"][q_level]),
+                        ";".join("{}".format(a["text"]) for a in q),
                     )
-                )
+                    print(
+                        text_csv,
+                        file=f,
+                    )
+                    text = "q={:d} in:{:>12.4f} out:{:>5.2f}".format(
+                        q_level, out["x0"][s_level], out["out"][q_level]
+                    )
+                    text += "".join(
+                        " {} = {}".format(a, b["text"])
+                        for a, b in zip(name_of_the_machines, q)
+                    )
+                    temp_res["x0"] = out["x0"][s_level]
+                    temp_res["q_level"] = q_level
+                    temp_res["text"] = text
+                    temp_res["text_csv"] = text_csv
 
-        print()
-        print("==================")
-        print("q_level = {}".format(q_level))
-        print()
-        print_res(res[q_level], q_level)
-    return res
+                    if temp_res["x0"] < min_res[q_level]["x0"]:
+                        min_res[q_level] = dict(temp_res)
+
+    for q in range(5):
+        print(min_res[q]["text"])
+
+    return min_res
 
 
-q = "Normal"
+# q = "Normal"
 # q = "Uncommon"
-q = "Rare"
+# q = "Rare"
 # q = "Epic"
 # q = "Legendary"
 
@@ -135,17 +171,19 @@ q = "Rare"
 # print("==================")
 # print("assembly machine")
 # print()
-# make_a_complete_search(
-#     [1.0, 0, 0, 0, 0],
-#     scheme_1(),
-#     ("machine", "recycler", "machine"),
-#     [
-#         get_q_list(4, "T3", q, False),
-#         get_q_list_Qonly(4, "T3", q, False),
-#         get_q_list_Ponly(4, "T3", q, False),
-#     ],
-#     q_level_list=[1, 2],
-# )
+# for q in ("Normal", "Uncommon", "Rare", "Epic", "Legendary"):
+#     make_a_complete_search(
+#         [1.0, 0, 0, 0, 0],
+#         scheme_1(),
+#         ("machine", "recycler", "machine"),
+#         [
+#             get_q_list(4, "T3", q, False),
+#             get_q_list_Qonly(4, "T3", q, False),
+#             get_q_list_Ponly(4, "T3", q, False),
+#         ],
+#         q_level_list=[0, 1, 2, 3, 4],
+#         filename="out_{}.csv".format(q)
+#     )
 
 # print()
 # print("==================")
@@ -163,8 +201,30 @@ q = "Rare"
 #     q_level_list=[1, 4],
 # )
 
+print()
+print("==================")
+print("drill -> furn -> GC (iron only)")
+print()
+for q in ("Normal", "Uncommon", "Rare", "Epic", "Legendary"):
+    make_a_complete_search(
+        [1.0, 0, 0, 0, 0],
+        scheme_4(),
+        ("drill", "furn1", "furn2", "machGC1", "machGC2", "machT11", "rec", "machT12"),
+        [
+            get_q_list_Qonly(3, "T3", q, False),  # drill
+            get_q_list(2, "T3", q, False),  # furn1
+            get_q_list_Ponly(2, "T3", q, False),  # furn2
+            get_q_list(4, "T3", q, False),  # ass GC1
+            get_q_list_Ponly(4, "T3", q, False),  # ass GC2
+            get_q_list_Qonly(4, "T3", q, False),  # T1
+            get_q_list_Qonly(4, "T3", q, False),  # recycler
+            get_q_list_Qonly(4, "T3", q, False),  # T1
+        ],
+        q_level_list=[0, 2],
+        filename="out_{}.csv".format(q),
+    )
 
-# q = "Rare"
+
 # q = "Legendary"
 # print()
 # print("==================")
@@ -173,38 +233,17 @@ q = "Rare"
 # make_a_complete_search(
 #     [1.0, 0, 0, 0, 0],
 #     scheme_3(),
-#     ("drill", "furn1", "furn2", "machine", "rec", "machine"),
+#     ("drill", "furn1", "furn2", "EMP", "rec", "EMP"),
 #     [
 #         get_q_list_Qonly(3, "T3", q, False),  # drill
 #         get_q_list(2, "T3", q, False),  # furn1
 #         get_q_list_Ponly(2, "T3", q, False),  # furn2
-#         get_q_list(4, "T3", q, False),  # ass GC
+#         get_q_list(5, "T3", q, True),  # ass GC
 #         get_q_list_Qonly(4, "T3", q, False),  # recycler
-#         get_q_list_Ponly(4, "T3", q, False),  # ass GC
+#         get_q_list_Ponly(5, "T3", q, True),  # ass GC
 #     ],
 #     q_level_list=[4],
 # )
-
-
-q = "Legendary"
-print()
-print("==================")
-print("drill -> furn -> GC (iron only)")
-print()
-make_a_complete_search(
-    [1.0, 0, 0, 0, 0],
-    scheme_3(),
-    ("drill", "furn1", "furn2", "EMP", "rec", "EMP"),
-    [
-        get_q_list_Qonly(3, "T3", q, False),  # drill
-        get_q_list(2, "T3", q, False),  # furn1
-        get_q_list_Ponly(2, "T3", q, False),  # furn2
-        get_q_list(5, "T3", q, True),  # ass GC
-        get_q_list_Qonly(4, "T3", q, False),  # recycler
-        get_q_list_Ponly(5, "T3", q, True),  # ass GC
-    ],
-    q_level_list=[4],
-)
 
 # make_a_complete_search(
 #     get_q_list(5, "T3", "Legendary", True),
